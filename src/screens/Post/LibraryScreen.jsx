7049aaa2cd13ec48781f339e01797d1b4e1a187d
Phamhuuphuc2001 <aresgaming607@gmail.com>
2024-01-23 17:29:05 +0700

    updatelibra

--- src/screens/Post/LibraryScreen.jsx
+++ src/screens/Post/LibraryScreen.jsx
@@ -1,54 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { View, FlatList, Image, StyleSheet } from 'react-native';
-// import * as MediaLibrary from 'expo-media-library';
-
-// const LibraryScreen = () => {
-//   const [libraryImages, setLibraryImages] = useState([]);
-
-//   const fetchLibraryImages = async () => {
-//     const { assets } = await MediaLibrary.getAssetsAsync({
-//       mediaType: MediaLibrary.MediaType.photo,
-//       first: 20, // adjust the number based on your needs
-//     });
-
-//     setLibraryImages(assets);
-//   };
-
-//   useEffect(() => {
-//     fetchLibraryImages();
-//   }, []); // Fetch library images when the component mounts
-
-//   return (
-//     <View style={styles.container}>
-//       <FlatList
-//         data={libraryImages}
-//         keyExtractor={(item) => item.id}
-//         renderItem={({ item }) => (
-//           <Image
-//             source={{ uri: item.uri }}
-//             style={styles.image}
-//           />
-//         )}
-//         numColumns={3}
-//       />
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 8,
-//   },
-//   image: {
-//     width: 100,
-//     height: 100,
-//     margin: 4,
-//   },
-// });
-
-// export default LibraryScreen;
-
 import React, { useState, useEffect } from 'react';
 import { View, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import * as MediaLibrary from 'expo-media-library';
@@ -74,7 +23,7 @@ const LibraryScreen = () => {
 
       const { assets } = await MediaLibrary.getAssetsAsync({
         mediaType: MediaLibrary.MediaType.photo,
-        first: 20, // adjust the number based on your needs
+        first: 20,
       });
 
       setLibraryImages(assets);
@@ -85,21 +34,16 @@ const LibraryScreen = () => {
 
   useEffect(() => {
     fetchLibraryImages();
-  }, []); // Fetch library images when the component mounts
+  }, []);
 
   const goBack = () => {
-    // Sử dụng navigation.goBack() để quay lại trang trước
     navigation.goBack();
   };
   const handleImagePress = (item) => {
-    // Toggle selectedImage state when an image is pressed
     setSelectedImage(selectedImage === item ? null : item);
   };
   const handleNextPress = () => {
-    // Handle the "Tiếp theo" button press
     if (selectedImage) {
-      // Perform any action needed for the selected image
-      // For example, navigate to the next screen
       navigation.navigate(ROUTES.UPPOST, { selectedImage });
     }
   };
@@ -120,12 +64,6 @@ const LibraryScreen = () => {
         data={libraryImages}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          // <TouchableOpacity onPress={() => navigation.navigate(ROUTES.UPPOST)}>
-          //   <Image
-          //     source={{ uri: item.uri }}
-          //     style={styles.image}
-          //   />
-          // </TouchableOpacity>
           <TouchableOpacity onPress={() => handleImagePress(item)}>
             <Image
               source={{ uri: item.uri }}
